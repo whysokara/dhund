6a1a9ac6e5c2f85428134ee83a4f427e08eb10d7
Kara <kara_himanshu@yahoo.com>
2022-01-06 23:48:30 +0530

    Dark Mode Added

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
