a035022c238a9ed6f154c74eeb695bfe18f0f919
Kara <kara_himanshu@yahoo.com>
2022-01-07 11:58:38 +0530

    config bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@
 //   plugins: [],
 // };
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
