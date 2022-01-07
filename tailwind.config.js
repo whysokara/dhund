9217695468fb3c13a91d3f1ce865d25faf13468e
Kara <kara_himanshu@yahoo.com>
2022-01-07 12:03:35 +0530

    changes reversed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@
 //   plugins: [],
 // };
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
